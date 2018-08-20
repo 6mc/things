b58e33e091c4f4fc97ce1f6a142f073d052e14c2
Mehmet Can <super.can@bk.ru>
2018-08-20 23:50:47 +0300

    zagros framework practices

---
+++ zagros framework/sayfa/anasayfa.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<script type="text/javascript">
+	
+	var val=getElementsById('');
+
+
+</script>
+</body>
+</html>
+
+<pre>
+
+
+<?php  
+//echo "Hoş geldin";
+
+$hello="hoşgeldin";
+
+echo $hello;
+z(6,"firma");
+echo strlen($hello);
+ $_Firma =z(1, ['ad'=>z('get','isim')], 'ad,telno');
+ print_r($_Firma);
+//z(2,["ad"=>"netadım","telno"=>"05552221100"]);
+//_z(1);
+
+
+ _z(3,2,Array('ad'=>'ayb','telno'=>'053322211122')); 
+
+
+
+
+?>
+
+
+
