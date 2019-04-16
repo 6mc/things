63a62267545fa19ea746ca458faa77fa3431b0ae
Mehmet Can <super.can@bk.ru>
2019-04-16 11:51:32 +0300

    Add files via upload

--- yorone/import.bat
+++ yorone/import.bat
@@ -2,5 +2,11 @@ FBZip.exe -p -E hacking-terminal.zip ./yorone
 cd yorone
 ren css hac
 ren js  ha
-cd ..
+MORE /E +3 original.csv > new.csv rem delete first 3 rows of the html file
+echo "my line" > newFile.txt      rem appending css files to beginning of the html files
+type myOriginalFile.txt >> newFile.txt
+type newFile.txt > myOriginalFile.txt
+REM for %%I in (.) do set CurrDirName=%%~nxI
+REM echo %CurrDirName%
+REM set /p temp="Hit enter to continue"
 
