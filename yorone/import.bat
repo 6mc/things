d242547e08dbe86d58542f85f9123d36d48c30a8
Mehmet Can <super.can@bk.ru>
2019-04-18 11:42:08 +0300

    Add files via upload

--- yorone/import.bat
+++ yorone/import.bat
@@ -1,14 +1,14 @@
-FBZip.exe -p -E hacking-terminal.zip ./yorone
-cd yorone
-ren css hac
-ren js  ha
-
-REM  
-
 @echo off
+set filename=%1
+set ilk=%filename:~0,5%
+echo %ilk%  
+
+FBZip.exe -p -E %filename% ./%ilk%
+
+
     setlocal enableextensions disabledelayedexpansion
 
-    for %%a in ("text1.txt") do > "%%~dpna_copy%%~xa" (
+    for %%a in ("index.html") do > "%%~dpna_copy%%~xa" (
         set "line="
         for /f "skip=1 tokens=* delims=0123456789" %%a in ('
             findstr /n /r "^" "%%a"
@@ -22,11 +22,27 @@ REM
         )
     )
 
-    echo "my line" > newFile.txt      rem appending css files to beginning of the html files
-type myOriginalFile.txt >> newFile.txt
-type newFile.txt > myOriginalFile.txt
+    del index.html
+    ren index_copy.html index.html
 
-echo "src-index.js" >> index.html
+    echo "<link rel='stylesheet' href='%ilk%/css/style.css'>" > newFile.txt    
+      rem appending css files to beginning of the html files
+type index.html >> newFile.txt
+type newFile.txt > index.html
+
+    echo "<html>" > newFile.txt      
+    rem appending <html> files to beginning of the html files
+type index.html >> newFile.txt
+type newFile.txt > index.html
+
+echo "<script src='%ilk%/js/index.js'></script>" >> index.html 
+rem appending js files to end of file
+
+
+echo "</html>" >> index.html
+ rem appending /</html> to end of file
+
+
 REM for %%I in (.) do set CurrDirName=%%~nxI
 REM echo %CurrDirName%
 REM set /p temp="Hit enter to continue"
---
+++ yorone/test.bat
@@ -0,0 +1,4 @@
+@echo off
+set filename=%1
+set nagmes=%filename:~0,5%
+echo %nagmes%  
