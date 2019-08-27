9389ec5dbdf83d32ced3b6925e268bc027df77ff
Mehmet Can <super.can@bk.ru>
2019-08-27 16:18:35 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -16,41 +16,72 @@ soup = BeautifulSoup(page.content, 'html.parser')
 # print(soup.find_all('figcaption')[0].get_text())
 link_list = [a['href'] for a in soup.find_all('a', href=True)]
 
-print(link_list[70])
+# print(link_list[70])
 
 # app = requests.get(link_list[70])
 
-details = BeautifulSoup(requests.get(link_list[70]).content, 'html.parser')
+for x in xrange(70,170):
+	print(x)
+	details = BeautifulSoup(requests.get(link_list[x]).content, 'html.parser')
+	title = details.find('title').get_text()[:-16] 
+	title = title[1:]
+	sheet1.write(x-69, 0, title)
+	sheet1.write(x-69, 1, link_list[70])
+	properties= details.find_all('dd');
+	try:
+		print( properties[8].get_text().strip())
+		sheet1.write(x-69, 2, properties[8].get_text().strip())
+	except Exception as e:
+		print("The App has 8 variables")
+	for y in xrange(0,7):
+		sheet1.write(x-69, y+3, properties[y].get_text().strip()) 
+	try:
+		print( properties[8].get_text().strip())
+		sheet1.write(x-69, 11, properties[8].get_text().strip())
+	except Exception as e:
+		print("The App has 8 variables")		
+	try:
+		print( properties[9].get_text().strip())
+		sheet1.write(x-69, 12, properties[9].get_text().strip())
+	except Exception as e:
+		print("The App has 8 variables")
+	try:
+		print( properties[10].get_text().strip())
+		sheet1.write(x-69, 13, properties[10].get_text().strip())
+	except Exception as e:
+		print("The App has 8 variables")
+	
+# details = BeautifulSoup(requests.get(link_list[70]).content, 'html.parser')
 
-title = details.find('title').get_text()[:-16] 
+# title = details.find('title').get_text()[:-16] 
 
-title = title[1:]
+# title = title[1:]
 
-sheet1.write(1, 0, title)
-sheet1.write(1, 1, link_list[70])
+# sheet1.write(1, 0, title)
+# sheet1.write(1, 1, link_list[70])
 
-print( title)
+#print( title)
 
-properties= details.find_all('dd');
+# properties= details.find_all('dd');
 
-print( properties[0].get_text().strip())
-print( properties[1].get_text().strip())
-print( properties[2].get_text().strip())
-print( properties[3].get_text().strip())
-print( properties[4].get_text().strip())
-print( properties[5].get_text().strip())
-print( properties[6].get_text().strip())
-print( properties[7].get_text().strip())
-try:
-	print( properties[8].get_text().strip())
-	sheet1.write(1, 2, properties[8].get_text().strip())
-except Exception as e:
-	print("The App has 8 variables")
+# print( properties[0].get_text().strip())
+# print( properties[1].get_text().strip())
+# print( properties[2].get_text().strip())
+# print( properties[3].get_text().strip())
+# print( properties[4].get_text().strip())
+# print( properties[5].get_text().strip())
+# print( properties[6].get_text().strip())
+# print( properties[7].get_text().strip())
+# try:
+# 	print( properties[8].get_text().strip())
+# 	sheet1.write(1, 2, properties[8].get_text().strip())
+# except Exception as e:
+# 	print("The App has 8 variables")
 
 		
         
 
-for x in xrange(0,8):
-	sheet1.write(1, x+3, properties[x].get_text().strip()) 
-	pass
-wb.save('Appstore.xls') 
+# for x in xrange(0,8):
+# 	sheet1.write(1, x+3, properties[x].get_text().strip()) 
+# 	pass
+wb.save('Apps.xls') 
