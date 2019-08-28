13aaea93272630ba5f87fc0d44350b8f8f6aae9e
Mehmet Can <super.can@bk.ru>
2019-08-29 01:43:43 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -16,48 +16,65 @@ soup = BeautifulSoup(page.content, 'html.parser')
 # print(soup.find_all('figcaption')[0].get_text())
 link_list = [a['href'] for a in soup.find_all('a', href=True)]
 
-# print(link_list[70])
-print(len(link_list))
-# app = requests.get(link_list[70])
+counter=0
 
-for x in xrange(70,72):
-	print(x)
-	details = BeautifulSoup(requests.get(link_list[x]).content, 'html.parser')
-	title = details.find('title').get_text()[:-16] 
-	title = title[1:]
-	sheet1.write(x-69, 0, title)
-	sheet1.write(x-69, 1, link_list[x])
-	properties= details.find_all('dd')
-	image = details.find_all('source')[2]
-	sheet1.write(x-69, 14, image['srcset'].split(" ")[0])
-	try:		
-		sheet1.write(x-69, 2, details.find_all('figcaption')[0].get_text().strip())
-	except Exception as e:
-		print("The App has 8 variables")
-	for y in xrange(0,7):
-#		sheet1.write(x-69, y+3, properties[y].get_text().strip())
-		try:
-			sheet1.write(x-69, y+3, properties[y].get_text().strip())
-		except Exception as e:
-			print("No internet")
-	try:
-		print( properties[8].get_text().strip())
-		sheet1.write(x-69, 11, properties[8].get_text().strip())
-	except Exception as e:
-		print("The App has 8 variables")		
-	try:
-		print( properties[9].get_text().strip())
-		sheet1.write(x-69, 12, properties[9].get_text().strip())
-	except Exception as e:
-		print("The App has 8 variables")
-	try:
-		print( properties[10].get_text().strip())
-		sheet1.write(x-69, 13, properties[10].get_text().strip())
-	except Exception as e:
-		print("The App has 8 variables")
+for pn in xrange(106,109):
 	
-# details = BeautifulSoup(requests.get(link_list[70]).content, 'html.parser')
+	if len(link_list)<286:
+		break		
+	
+	page = requests.get("https://apps.apple.com/tr/genre/ios-games/id6014?letter=A&page=" + str(pn) +"#page")
 
+	soup = BeautifulSoup(page.content, 'html.parser')
+	# print(soup.find_all('figcaption')[0].get_text())
+	link_list = [a['href'] for a in soup.find_all('a', href=True)]
+
+	# print(link_list[70])
+	#print(len(link_list))
+	# app = requests.get(link_list[70])
+
+	for x in xrange(71,166):
+		counter=counter+1
+		print(counter)
+		details = BeautifulSoup(requests.get(link_list[x]).content, 'html.parser')
+		title = details.find('title').get_text()[:-16] 
+		title = title[1:]
+		sheet1.write(counter, 0, title)
+		sheet1.write(counter, 1, link_list[x])
+		properties= details.find_all('dd')
+
+		try:		
+			image = details.find_all('source')[0]
+			sheet1.write(counter, 14, image['srcset'].split(" ")[0])
+		except Exception as e:
+			print("The App doesnt have an icon")
+		try:		
+			sheet1.write(counter, 2, details.find_all('figcaption')[0].get_text().strip())
+		except Exception as e:
+			print("The App has 8 variables")
+		for y in xrange(0,7):
+	#		sheet1.write(counter, y+3, properties[y].get_text().strip())
+			try:
+				sheet1.write(counter, y+3, properties[y].get_text().strip())
+			except Exception as e:
+				print("No internet")
+		try:
+			print( properties[8].get_text().strip())
+			sheet1.write(counter, 11, properties[8].get_text().strip())
+		except Exception as e:
+			print("The App has 8 variables")		
+		try:
+			print( properties[9].get_text().strip())
+			sheet1.write(counter, 12, properties[9].get_text().strip())
+		except Exception as e:
+			print("The App has 8 variables")
+		try:
+			print( properties[10].get_text().strip())
+			sheet1.write(counter, 13, properties[10].get_text().strip())
+		except Exception as e:
+			print("The App has 8 variables")
+		
+	# details = BeautifulSoup(requests.get(link_list[70]).content, 'html.parser')
 # title = details.find('title').get_text()[:-16] 
 
 # title = title[1:]
